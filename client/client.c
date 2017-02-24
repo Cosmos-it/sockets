e92b96a94bfd00eb0df22d44e9dddb3835a93fce
Taban Cosmos <Cosmos-it>
2017-02-24 01:52:20 -0800

    Update server and client

--- client/client.c
+++ client/client.c
@@ -5,33 +5,41 @@
 //  Copyright © 2017 Taban Cosmos. All rights reserved.
 
 #include <arpa/inet.h>
+#include <math.h>
+#include <netdb.h>
 #include <netinet/in.h>
 #include <pthread.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
-#include <stdlib.h>
-#include <netdb.h>
 
 /* Packet struct */
 struct packet {
   int x;
   int y;
 };
+
+struct results {
+  int result;
+};
+
 void checkPrime(int x, int y);
 void *threadFunction(void *socket);
 
 // Global variables:
 int clientSocket;
 struct packet pk;
+struct results rst;
 pthread_t td;
+const int running = 1;
 
 /* Main */
 int main() {
+  struct sockaddr_in serverAddr;
   char *ipAddress = "127.0.0.1";
   int x, y, i = 1;
-  struct sockaddr_in serverAddr;
   socklen_t addr_size;
 
   /*---- Create the socket. The three arguments are: ----*/
@@ -45,14 +53,14 @@ int main() {
   memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);
   /* Connect the socket to server*/
   addr_size = sizeof serverAddr;
-  if (connect(clientSocket, (struct sockaddr *)&serverAddr, addr_size) < 0)
-  {
-    perror("Can't set remote->sin_addr.s_addr");
-    exit(1);
+  if (connect(clientSocket, (struct sockaddr *)&serverAddr, addr_size) < 0) {
+    perror("Error connections");
+    exit(running);
   }
-
-  while (1) {
+  while (running) {
+    /* code */
     threadFunction(&clientSocket);
+    send(clientSocket, &(rst), sizeof(rst), 0);
   }
   pthread_exit(NULL);
 
@@ -60,26 +68,25 @@ int main() {
 }
 
 void *threadFunction(void *socket) {
-    /*---- Print the received message ----*/
-    while (1) {
-      /*---- Read the message from struct packet ----*/
-      recv(clientSocket, &(pk), sizeof(pk), 0);
-      printf("Data received: %d\n", pk.x);
-      printf("Data received Sqrt: %d\n", pk.y);
-      checkPrime(pk.x, pk.y); // Check for prime numbers
-      pthread_create(&td, NULL, threadFunction, (void *)&socket);
-
-    }
+  /*---- Print the received message ----*/
+  while (running) {
+    /*---- Read the message from struct packet ----*/
+    recv(clientSocket, &(pk), sizeof(pk), 0);
+    printf("Data received: %d\n", pk.x);
+    printf("Data received Sqrt: %d\n", pk.y);
+    checkPrime(pk.x, pk.y); // Check for prime numbers
+    pthread_create(&td, NULL, threadFunction, (void *)&socket);
+  }
 }
 
 /* Check for primes */
 void checkPrime(int x, int y) {
   /* code */
   if (x % y == 0) {
-    printf("%d %s\n", x, ": is a prime number");
-  } else if (x / y == y) {
-    printf("%d %s %d\n", x / y, ": equals to y: ", y);
-  } else {
-    printf("%d %s\n", x, ": is not a prime");
+    rst.result = x;
+    printf("%d %s\n", rst.result, ": not prime numnber");
+  } else if (y == sqrt(x)) {
+    rst.result = y;
+    printf("%d %s\n", rst.result, ": not prime numnber");
   }
 }
--- server/server.c
+++ server/server.c
@@ -14,6 +14,18 @@
 #include <sys/socket.h>
 #include <time.h>
 
+/* Function declaration */
+long randomGenerator();
+
+/* Struct*/
+struct packet {
+  int x;
+  int y;
+};
+struct results {
+  int result;
+};
+
 /* Random generator */
 long randomGenerator() {
   time_t t;
@@ -21,17 +33,16 @@ long randomGenerator() {
   return rand() % 800 + 777;
 }
 
-/* Struct*/
-struct packet { int x; int y; };
-
 int main() {
   struct packet pk;
+  struct results rst;
   int welcomeSocket, newSocket;
   char *ipAddress = "127.0.0.1";
   struct sockaddr_in serverAddr;
   struct sockaddr_storage serverStorage;
   socklen_t addr_size;
   const int MAX_CONNECTION = 10;
+  const int running = 1;
 
   welcomeSocket = socket(PF_INET, SOCK_STREAM, 0);
   serverAddr.sin_family = AF_INET;
@@ -44,23 +55,29 @@ int main() {
   /*---- Bind the address struct to the socket ----*/
   bind(welcomeSocket, (struct sockaddr *)&serverAddr, sizeof(serverAddr));
 
-
-  while (1) { //Client is allowed to make request to this part many times
-    pk.x = (int)randomGenerator(); //Get a huge generated number
-    pk.y = sqrt((int)randomGenerator()); //Get sqrt of the generated number
+  while (running) { // Client is allowed to make request to this part many times
+    pk.x = (int)randomGenerator();       // Get a huge generated number
+    pk.y = sqrt((int)randomGenerator()); // Get sqrt of the generated number
 
     /* Test the out puts */
-    printf("Original: %d\n", pk.x); //Test values for x
-    printf("Sqrt of Original: %d\n", pk.y); //Test values for y
+    printf("Original: %d\n", pk.x);         // Test values for x
+    printf("Sqrt of Original: %d\n", pk.y); // Test values for y
 
     if (listen(welcomeSocket, MAX_CONNECTION) == 0)
-      printf("Listening %s\n", ipAddress); //Show the address server is listening at
+      printf("Listening %s\n",
+             ipAddress); // Show the address server is listening at
     else
-    printf("Error\n"); //Print error if there is connection issues
+      printf("Error\n"); // Print error if there is connection issues
+    // if (rst.result % 2 != 0)
+    //   break;
 
     addr_size = sizeof serverStorage;
-    newSocket = accept(welcomeSocket, (struct sockaddr *)&serverStorage, &addr_size);
+    newSocket =
+        accept(welcomeSocket, (struct sockaddr *)&serverStorage, &addr_size);
     send(newSocket, &(pk), sizeof(pk), 0); // send the packet to the client.
+    int data = recv(welcomeSocket, &(rst), sizeof(rst), 0);
+    printf("Data received: %d\n", data);
+    printf("Prime Check: %d\n", rst.result);
   }
 
   return 0;
